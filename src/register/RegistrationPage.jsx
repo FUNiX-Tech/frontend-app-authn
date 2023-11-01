1d2725b9cf693b6b9921bc6c578144b81a7ff974
sonxauxi2411 <112046655+sonxauxi2411@users.noreply.github.com>
2023-11-01 09:08:41 +0700

    a

--- src/register/RegistrationPage.jsx
+++ src/register/RegistrationPage.jsx
@@ -303,7 +303,7 @@ class RegistrationPage extends React.Component {
 
     // add query params to the payload
     payload = { ...payload, ...this.queryParams };
-    // console.log('======payload=====', payload)
+    console.log('======payload=====', payload)
     this.setState({
       totalRegistrationTime,
     }, () => {
